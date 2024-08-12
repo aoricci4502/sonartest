daffc2164d331df2055f64742d9659f87bb74d0e
Alberto <166945719+aoricci6@users.noreply.github.com>
2024-08-12 10:32:53 -0300

    Update sonar.py

--- sonar.py
+++ sonar.py
@@ -1,7 +1,7 @@
 # el que mira aprende :D
 import random
 import time
-
+#update
 while True:
     numero_aleatorio = random.randint(1, 100)  # Genera un nÃºmero aleatorio entre 1 y 100
     print(f"NÃºmero aleatorio: {numero_aleatorio}")
